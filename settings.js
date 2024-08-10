cdea5616828f23b5d3ee4c326a9c200b18b094ca
kuttapa112 <91403845+kuttapa112@users.noreply.github.com>
2024-08-10 02:07:27 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923334904751"
+global.ownername = "🐛 MR FASU "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
+global.ownernumber = '923334904751'  //creator number
 global.ownername = ' MR WASI ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
